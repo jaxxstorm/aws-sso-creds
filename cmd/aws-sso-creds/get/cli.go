76c7d420a9039462e42308c9f53a650d158751c2
Lee Briggs <lee@leebriggs.co.uk>
2020-07-09 19:52:52 -0700

    working credentials package

--- cmd/aws-sso-creds/get/cli.go
+++ cmd/aws-sso-creds/get/cli.go
@@ -2,13 +2,10 @@ package get
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sso"
-	"github.com/jaxxstorm/aws-sso-creds/pkg/config"
+
+	"github.com/jaxxstorm/aws-sso-creds/pkg/credentials"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"io/ioutil"
 )
 
 func Command() *cobra.Command {
@@ -25,40 +22,19 @@ func Command() *cobra.Command {
 				return fmt.Errorf("no profile specified")
 			}
 
-			ssoConfig, err := config.GetSSOConfig(profile)
-			if err != nil {
-				fmt.Errorf("error retrieving SSO config: %w", err)
-			}
-
-			cacheFiles, err := ioutil.ReadDir(fmt.Sprintf("%s/.aws/sso/cache", homeDir))
-			if err != nil {
-				fmt.Errorf("error retrieving cache files - perhaps you need to login?: %w", err)
-			}
-
-			token, err := config.GetSSOToken(cacheFiles, *ssoConfig)
-			if err != nil {
-				panic(err)
-			}
-
-			sess := session.Must(session.NewSession())
-			svc := sso.New(sess, aws.NewConfig().WithRegion(ssoConfig.Region))
-
-			creds, err := svc.GetRoleCredentials(&sso.GetRoleCredentialsInput{
-				AccessToken: &token,
-				AccountId: &ssoConfig.AccountID,
-				RoleName: &ssoConfig.RoleName,
-			})
+			creds, err := credentials.GetSSOCredentials(profile, homeDir)
 
 			if err != nil {
-				panic(err)
+				return err
 			}
 
-			fmt.Println(creds)
+			fmt.Printf("export AWS_ACCESS_KEY_ID=%s\n", *creds.RoleCredentials.AccessKeyId)
+			fmt.Printf("export AWS_SECRET_ACCESS_KEY=%s\n", *creds.RoleCredentials.SecretAccessKey)
+			fmt.Printf("export AWS_SESSION_TOKEN=%s\n", *creds.RoleCredentials.SessionToken)
 
 			return nil
 		},
 	}
-
 
 	return command
 }
---
+++ pkg/credentials/creds.go
@@ -0,0 +1,45 @@
+package credentials
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sso"
+	"github.com/jaxxstorm/aws-sso-creds/pkg/config"
+)
+
+func GetSSOCredentials(profile string, homedir string) (*sso.GetRoleCredentialsOutput, error) {
+
+	ssoConfig, err := config.GetSSOConfig(profile)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving SSO config: %w", err)
+	}
+
+	cacheFiles, err := ioutil.ReadDir(fmt.Sprintf("%s/.aws/sso/cache", homedir))
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving cache files - perhaps you need to login?: %w", err)
+	}
+
+	token, err := config.GetSSOToken(cacheFiles, *ssoConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving SSO token from cache files: %w", err)
+	}
+
+	sess := session.Must(session.NewSession())
+	svc := sso.New(sess, aws.NewConfig().WithRegion(ssoConfig.Region))
+
+	creds, err := svc.GetRoleCredentials(&sso.GetRoleCredentialsInput{
+		AccessToken: &token,
+		AccountId:   &ssoConfig.AccountID,
+		RoleName:    &ssoConfig.RoleName,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving credentials from AWS: %w", err)
+	}
+
+	return creds, nil
+
+}
