a9d30251c3e6095b3f8a01ae099ba33ea1146f39
DucThuan Nguyen <ducthuan.nguyen@nfq.com>
2023-06-15 16:28:27 +0700

    Fix default profile support

--- cmd/aws-sso-creds/set/cli.go
+++ cmd/aws-sso-creds/set/cli.go
@@ -81,7 +81,10 @@ func Command() *cobra.Command {
 				return fmt.Errorf("error saving credentials file: %v", err)
 			}
 
-			configSection := "profile " + section
+			configSection := section
+			if section != "default" {
+				configSection = "profile " + section
+			}
 			if !configFile.HasSection(configSection) {
 				if err := configFile.AddSection(configSection); err != nil {
 					return fmt.Errorf("error creating credentials section in config file: %v", err)
